7a17484c0c5eed298ec4ef8ba031f646767c7ada
Ameer Jhan <ameerjhanprof@gmail.com>
2019-07-09 09:46:17 +0530

    chore: initial commit

---
+++ CrazyWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrazyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("WhoAreYou")]
+        public ActionResult<string> Get()
+        {
+            return "I'm Crazy";
+        }
+    }
+}
